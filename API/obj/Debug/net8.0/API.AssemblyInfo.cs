8be102f6bccc8c1e0696f82f8d53cbb85365843f
paracetamol <140186933+apoo3301@users.noreply.github.com>
2024-11-17 21:10:10 +0100

    /api/Roles post endpoint added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519a407749a8b3f79ec684ba11fcdf7bf92c130d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586ef8cdb895ae7face02eece1b78d7e65a59d59")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
